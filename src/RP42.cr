06acc626cd57974007e98920b7d8f01a0f1bba8b
aguiot-- <aguiot--@student.42.fr>
2019-01-29 18:10:34 +0100

    Add level, coalition and login

--- src/RP42.cr
+++ src/RP42.cr
@@ -1,20 +1,36 @@
+require "oauth2"
 require "./client"
 
 module RP42
 	VERSION = "0.1.0"
 
-  hostname = System.hostname.split(".", 2)
-  exit if hostname[1] != "42.fr" 
+  api_id = "6006639307c5ecc1506feb96d9f833df325e07573040acecad429f75b824a72e"
+  api_secret = "<API-SECRET>"
 
-  client = RichCrystal::Client.new(531103976029028367_u64)
-	client.login
-	client.activity({
-		"details" => "Location: #{hostname[0]}",
+  oauth2_client = OAuth2::Client.new("api.intra.42.fr", api_id, api_secret, authorize_uri: "/oauth/authorize", token_uri: "/oauth/token")
+  access_token = oauth2_client.get_access_token_using_client_credentials
+  http_client = HTTP::Client.new("api.intra.42.fr", tls: OpenSSL::SSL::Context::Client.insecure)
+  access_token.authenticate(http_client)
+
+  username = `whoami`.chomp("\n")
+  hostname = System.hostname.split(".", 2)
+  hostname[0] = "At home" if hostname[1] != "42.fr"
+
+  coa = JSON.parse(http_client.get("/v2/users/#{username}/coalitions").body)[1]["name"].to_s
+  lvl = JSON.parse(http_client.get("/v2/users/#{username}").body)["cursus_users"][0]["level"].to_s
+  
+  rich_client = RichCrystal::Client.new(531103976029028367_u64)
+    rich_client.login
+    rich_client.activity({
+    	"details" => "Level: #{lvl}",
+    	"state" => "Location: #{hostname[0]}",
     "assets" => {
       "large_image" => "logo",
-      "large_text" => "42",
+      "large_text" => username,
+      "small_image" => coa.tr(" ", "-").downcase,
+      "small_text" => coa,
     }
-	})
+  })
 
   sleep
 end
