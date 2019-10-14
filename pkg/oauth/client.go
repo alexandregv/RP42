3492a30760c658f73d60ed9cd53342b16592d2af
aguiot-- <aguiot--@student.42.fr>
2019-10-14 21:14:13 +0200

    Use ldflags instead of env vars for client id/secret

--- pkg/oauth/client.go
+++ pkg/oauth/client.go
@@ -3,12 +3,14 @@ package oauth
 import (
 	"fmt"
 	"golang.org/x/net/context"
-	clientcreds "golang.org/x/oauth2/clientcredentials"
+	"golang.org/x/oauth2/clientcredentials"
 	"net/http"
-	"os"
 )
-
-const URL = "https://api.intra.42.fr"
+const API_URL = "https://api.intra.42.fr"
+var (
+	API_CLIENT_ID string
+	API_CLIENT_SECRET string
+)
 
 type Client struct {
 	*http.Client
@@ -16,15 +18,15 @@ type Client struct {
 
 func GetClient() *Client {
 	client := newClient(
-		os.Getenv("RP42_CLIENT_ID"),
-		os.Getenv("RP42_CLIENT_SECRET"),
-		fmt.Sprint(URL, "/oauth/token"),
+		API_CLIENT_ID,
+		API_CLIENT_SECRET,
+		fmt.Sprint(API_URL, "/oauth/token"),
 	)
 	return client
 }
 
 func newClient(client_id string, client_secret string, token_url string) *Client {
-	config := &clientcreds.Config{
+	config := &clientcredentials.Config{
 		ClientID:     client_id,
 		ClientSecret: client_secret,
 		TokenURL:     token_url,
