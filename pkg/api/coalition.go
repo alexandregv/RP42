0bca65dc8aaf605c18bf7c2a89c188ec7a789d2d
aguiot-- <aguiot--@student.42.fr>
2019-11-05 17:30:14 +0100

    Change comments in structs, making them easier to partially uncomment

--- pkg/api/coalition.go
+++ pkg/api/coalition.go
@@ -3,13 +3,11 @@ package api
 // Coalition represents a coalition from the 42's API.
 // Truncated to keep only useful entries.
 type Coalition struct {
-	//ID       int    `json:"id"`
+	//	ID       int    `json:"id"`
 	Name string `json:"name"`
 	Slug string `json:"slug"`
-	/*
-		ImageURL string `json:"image_url"`
-		Color    string `json:"color"`
-		Score    int    `json:"score"`
-		UserID   int    `json:"user_id"`
-	*/
+	//		ImageURL string `json:"image_url"`
+	//		Color    string `json:"color"`
+	//		Score    int    `json:"score"`
+	//		UserID   int    `json:"user_id"`
 }
--- pkg/api/location.go
+++ pkg/api/location.go
@@ -5,14 +5,14 @@ import "time"
 // Location represents a connection location from the 42's API.
 // Truncated to keep only useful entries.
 type Location struct {
-	ID       int         `json:"id"`
-	BeginAt  time.Time   `json:"begin_at"`
-	EndAt    time.Time   `json:"end_at"`
-	Primary  bool        `json:"primary"`
-	Floor    interface{} `json:"floor"`
-	Row      interface{} `json:"row"`
-	Post     interface{} `json:"post"`
-	Host     string      `json:"host"`
-	CampusID int         `json:"campus_id"`
-	User     User        `json:"user"`
+	//	ID       int         `json:"id"`
+	BeginAt time.Time `json:"begin_at"`
+	//	EndAt    time.Time   `json:"end_at"`
+	//	Primary  bool        `json:"primary"`
+	//	Floor    interface{} `json:"floor"`
+	//	Row      interface{} `json:"row"`
+	//	Post     interface{} `json:"post"`
+	Host string `json:"host"`
+	//	CampusID int         `json:"campus_id"`
+	//	User     User        `json:"user"`
 }
