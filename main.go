6a27352f8b0b6b7e3def43dfc44faa74d7996dd7
Reach <reach@42paris.fr>
2025-01-12 14:22:18 +0100

    docs(main): explain why main.go

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import "github.com/alexandregv/RP42/cmd"
 
+// CLI entrypoint of the program.
+// We use main.go and not directly cmd/ to allow a clean `go install github.com/alexandregv/RP42`
 func main() {
 	cmd.Execute()
 }
